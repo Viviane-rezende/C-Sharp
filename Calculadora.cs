ecda1da1d9b563fa590d58947819839f385bb671
VIVIANE REZENDE SILVA <viviane.rsilva46@senacsp.edu.br>
2025-02-28 14:02:29 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_a1
+{
+    internal class Calculadora
+    {
+
+
+        public void FuncaoSoma(int n1, int n2)
+        {
+            int resultado;
+            resultado = n1 + n2;
+            Console.WriteLine("O resultado da soma é " + resultado);
+
+        }
+
+        public void FuncaoSub(int n1, int n2)
+        {
+            int resultado;
+            resultado = n1 - n2;
+            Console.WriteLine("O resultado da subtração é " + resultado);
+
+        }
+
+        public void FuncaoMult(int n1, int n2)
+        {
+            int resultado;
+            resultado = n1 * n2;
+            Console.WriteLine("O resultado da multiplicação é " + resultado);
+
+        }
+
+        public void FuncaoDivisao(int n1, int n2)
+        {
+            int resultado;
+            resultado = n1 / n2;
+            Console.WriteLine("O resultado da divisão é " + resultado);
+
+        }
+
+        public void CalculaAreaRetangulo(int baseR, int altura)
+        {
+            int area;
+            area = baseR * altura;
+            Console.WriteLine("A área é: " + area);
+        }
+
+
+
+        public void Somar()
+        {
+            int n1, n2, resultado;
+            Console.WriteLine("Você escolheu somar! \nDigite o primeiro número");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            resultado = n1 + n2;
+            Console.WriteLine("O resultado da soma é: " + resultado);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,147 @@
+﻿using System.Data;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace C_Sharp_a1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Apresentacao ap1 = new Apresentacao();
+            Calculadora calculadora1 = new Calculadora();
+            ap1.Saudacao("Vivi");
+            //calculadora1.Soma();
+            calculadora1.FuncaoSoma(10, 02);
+            calculadora1.FuncaoSub(20, 10);
+            calculadora1.FuncaoMult(7, 7);
+            calculadora1.FuncaoDivisao(25, 25);
+            calculadora1.CalculaAreaRetangulo(20, 10);
+
+
+
+            String menu = "Qual operação deseja executar? \n1 para soma\n2 para subtração\n3 para divisão \n4 multiplicacão";
+            int op = 0;
+            do
+            {
+                Console.WriteLine(menu);
+                op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+
+                    case 1:
+
+                        calculadora1.Somar();
+
+                        break;
+                }
+            } while (op != 0);
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+/* int op;
+ String menu = "Qual operação deseja realizar?";
+
+ do
+ {
+     Console.WriteLine(menu);
+     switch (op)
+     {
+         case 1:
+
+     }
+ }
+}
+}
+}
+
+
+ //calculadora1.Soma();
+
+
+Program app = new Program();
+app.Saudacao("Oi");
+
+
+Program app1 = new Program();
+Program app2 = new Program();
+Apresentacao ap = new Apresentacao();
+
+
+
+}
+
+public void Saudacao(String nome)
+{
+Console.WriteLine("Hello World! " + nome) ;
+}
+
+
+
+
+float n1, n2, soma;
+
+
+String nome;
+int idade;
+DateTime dataHoje= DateTime.Now;
+
+Using 'WriteLine' to break lines
+
+Console.WriteLine("Viviane Rezende Silva");
+Console.WriteLine("29");
+Console.WriteLine("25/02/2025");
+
+// Using 'Write' that doesn't read lines and using '\n' to break lines
+
+Console.Write("\nViviane Rezende Silvaz\n");
+Console.Write("29\n");
+Console.Write("25/02/2025\n");
+
+Console.WriteLine("Qual é o seu nome?");
+nome = Console.ReadLine();                      //readline only read Strings
+
+Console.WriteLine("Qual a sua idade?");
+//idade= Int32.Parse(Console.ReadLine());
+idade = Convert.ToInt32(Console.ReadLine());    //here we are using the function Convert to convert Int to String and read it
+//idade = Int32.Parse(null);                    
+//idade = Convert.ToInt32(null);
+
+Console.WriteLine("A data de hoje é: "+dataHoje.ToString
+("dd/MM/yyyy"));
+
+Console.WriteLine(nome);
+Console.WriteLine(idade);
+
+Console.WriteLine("Seu nome é: "+nome+ ", sua idade é: "+idade+ " e a data de hoje é: "+dataHoje);
+
+
+Console.WriteLine("Digite o primeiro número:");
+n1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o segundo número");
+n2= Convert.ToInt32(Console.ReadLine());
+soma = n1 + n2;
+Console.WriteLine("O resultado da soma é: " + soma);
+Console.WriteLine($"O resultado {0} da soma é: " ,soma);
+
+
+Console.WriteLine("Digite o primeiro número:");
+n1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número");
+n2 = float.Parse(Console.ReadLine());
+soma = n1 + n2;
+Console.WriteLine("O resultado da soma é: " + soma);*/
