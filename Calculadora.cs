1cf062bec66c4e5a75be00478ccc289feb186a69
VIVIANE REZENDE SILVA <viviane.rsilva46@senacsp.edu.br>
2025-03-10 15:07:31 -0300

    Menu de Menus

--- Calculadora.cs
+++ Calculadora.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using C_Sharp;
 
 namespace C_Sharp_a1
 {
     internal class Calculadora
     {
 
+        public void Par()
+        {
+            int num;
+            Console.WriteLine("Digite um número");
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Número par!");
+            }
+            else
+            {
+                Console.WriteLine("Número ímpar");
+            }
+        }
 
         public void FuncaoSoma(int n1, int n2)
         {
@@ -94,5 +111,54 @@ namespace C_Sharp_a1
             resultado = n1 / n2;
             Console.WriteLine("O resultado da soma é: " + resultado);
         }
+
+       public void menuCalculadora()
+        {
+            Calculadora calculadora1 = new Calculadora();
+            String menu = "Digite o número referente a operação que deseja executar:\n\n1 para soma\n2 para subtração\n3 para multiplicação \n4 divisão \n5 para saber se é par ou ímpar \n0 para sair";
+            int op = 0;
+            do
+            {
+                Console.WriteLine("▒▒▒░░░░░░░░░░▄▐░░░░\r\n▒░░░░░░▄▄▄░░▄██▄░░░\r\n░░░░░░▐▀█▀▌░░░░▀█▄░\r\n░░░░░░▐█▄█▌░░░░░░▀█▄\r\n░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀\r\n░░░░░▄▄▄██▀▀▀▀░░░░░\r\n░░░░█▀▄▄▄█░▀▀░░░░░░\r\n░░░░▌░▄▄▄▐▌▀▀▀░░░░░\r\n░▄░▐░░░▄▄░█░▀▀░░░░░\r\n░▀█▌░░░▄░▀█▀░▀░░░░░\r\n░░░░░░░░▄▄▐▌▄▄░░░░░\r\n░░░░░░░░▀███▀█░▄░░░\r\n░░░░░░░▐▌▀▄▀▄▀▐▄░░░\r\n░░░░░░░▐▀░░░░░░▐▌░░\r\n░░░░░░░█░░░░░░░░█░░\r\n░░░░░░▐▌░░░░░░░░░█░");
+                Console.WriteLine(menu);
+                op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+
+                    case 1:
+
+                        calculadora1.Somar();
+                        break;
+                    case 2:
+
+                        calculadora1.Subtrair();
+                        break;
+                    case 3:
+
+                        calculadora1.Multiplicar();
+                        break;
+                    case 4:
+
+                        calculadora1.Dividir();
+                        break;
+                    case 5:
+
+                        calculadora1.Par();
+                        break;
+                    case 0:
+                        Console.WriteLine("Obrigado!!!");
+
+                        break;
+                }
+            } while (op != 0);
+
+
+
+
+
+
+        
+    }
     }
 }
+ 
